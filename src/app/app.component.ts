2c3f85092bdc62220ac4146e5b18e0da75b9adea
Grzegorz Eleryk <grzegorz.eleryk@gmail.com>
2024-02-12 09:15:51 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None,
+  host: {
+    class: 'app-root'
+  }
+})
+export class AppComponent {
+}
